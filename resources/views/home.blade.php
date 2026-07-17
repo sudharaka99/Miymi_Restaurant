fd7d5f30931fab02a49f00bdf92b633bf0135378
sudharaka99 <jrssudharaka@gmail.com>
2026-07-17 22:07:04 +0530

    load home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('title', 'Miyami restaurant - Fresh Food Every Day')
+@section('description', 'Miyami restaurant offers the best cuisine in town, with a focus on fresh ingredients and exceptional service.')
+@section('keywords', 'Miyami, restaurant, good food, cuisine, dining')
+
+@section('content')
+    @include('sections.offer')
+    @include('sections.food')
+    @include('sections.about')
+    @include('sections.book')
+    @include('sections.client')
+@endsection
